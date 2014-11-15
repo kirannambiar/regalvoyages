00105a36d1c9cd84eb44f577d0170a9acf5776cf
kirannambiar <kiran_nambiar@hotmail.com>
2014-11-15 15:29:29 -0500

    basic functionality to display Category headings and th list of posts per category are working.

--- front-page.php
+++ front-page.php
@@ -1,29 +1,41 @@
 <?php
 	get_header();
-	$args = array( 'category_name' => 'romantic,adventure', 'tag' => 'destination' );
-	//filtering on known categories
-	//$myfilter = 'category_name=romantic+adventure';
+	//$args = array( 'category_name' => 'romantic,adventure', 'tag' => 'destination', 'exculde' => '1' );
 	$myfilter = 'tag=destination';
-	$query = get_posts( $args );
+	$query = get_posts( $myfilter );
+    $destCategories = get_categories( array('tag' => 'destination' , 'exclude' => '1') );
+
 ?>
 
 <div id="main">
 	<div id="content clearfix">
 		<p>this is using front-page.php</p>
-		<?php echo var_dump($query) ?>
+		<?php //echo var_dump($destCategories) ?>
 		<?php 
 		if ( have_posts( $myfilter ) ) {
-			foreach ( $query as $post) : 
-				setup_postdata($post);
-				$t = wp_get_post_tags($post->ID, array( 'fields' => 'names' ));
-				echo "<p>tag for this post is + " + print_r($t) + "</p>";
-				?>
-			<h3><a href="<?php the_permalink(); ?>"> <?php echo the_title(); ?> </a></h3>
+			foreach ( $destCategories as $category ) :
+                //echo "<p>inside category foreach</p>";
+                //echo var_dump($category);
+                $posts = get_posts( array('category_name' => $category->slug) );
+                //echo var_dump($posts);
+                ?>
+                <h2><? echo $category->name ?></h2>
+                <?php
+				    foreach ( $posts as  $post ) :
+                        echo "<p>inside post foreach</p>";
+                        //echo var_dump($post);
+                        //setup_postdata($post);
+                        //$t = wp_get_post_tags($post->ID, array( 'fields' => 'names' ));
+				        //echo "<p>tag for this post is + " + print_r($t) + "</p>";
+                ?>
+                <h3><a href="<?php the_permalink(); ?>"> <?php echo the_title(); ?> </a></h3>
 
-			<!--<p> <?php the_content(); ?> </p>-->
-			
-		<?php endforeach;
-		wp_reset_postdata();
+			    <?php
+                    endforeach;
+                    wp_reset_postdata();
+			        //the_content();
+            endforeach;
+		//wp_reset_postdata();
 		}
 		else { ?>
 		<p>
--- functions.php
+++ functions.php
@@ -2,4 +2,17 @@
 
 	register_nav_menus();
 
+    // TODO: Remove this function as it's no longer used
+    function get_posts_by_categories($post_categories) {
+        $cats = array();
+
+        foreach($post_categories as $c) {
+            $cat = $c;
+            $cats[] = array('slug' => $cat->slug);
+        }
+        echo 'inside get posts !!!!!!!!!! ';
+        echo var_dump($cats);
+        return get_posts($cats);
+    }
+
 ?>
